368776147d5edcd092f5f1415f2da608b411251b
kitoumasaomi <kanfutrooper@gmail.com>
2025-09-21 15:44:01 +0900

    index.tsを作成

--- trello-clone-api/index.ts
+++ trello-clone-api/index.ts
@@ -1,20 +1,11 @@
 import express from 'express';
+import { AppDataSource } from './datasource';
 
 const app = express();
 const PORT = 8888;
 
 app.get('/', (req, res) => {
   res.send('Hello World');
-});
-
-//ルーティングを作成する
-app.get('/test', (req, res) => {
-  res.send('Hello World');
-});
-
-//URLパラメータとクエリパラメータを受け取る
-app.get('/users/:id', (req, res) => {
-  res.send(`User Id is ${req.params.id}.Name is ${req.query.name}`);
 });
 
 app.listen(PORT, () => {
