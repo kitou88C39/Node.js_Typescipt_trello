d08222edccf5fd44f9bef6a36b57b6bd94c75c9c
kitoumasaomi <kanfutrooper@gmail.com>
2025-09-21 15:38:52 +0900

    datasource.tsを作成

--- trello-clone-api/datasource.ts
+++ trello-clone-api/datasource.ts
@@ -1,3 +1,11 @@
 import { DataSource } from 'typeorm';
 
-export const AppDataSource = new DataSource();
+export const AppDataSource = new DataSource({
+  type: 'sqlite',
+  database: 'trello-clone.sqlite',
+  synchronize: true,
+  logging: false,
+  entities: ['src/entity/*.ts'],
+  migrations: ['src/migration/*.ts'],
+  subscribers: ['src/subscriber/*.ts'],
+});
